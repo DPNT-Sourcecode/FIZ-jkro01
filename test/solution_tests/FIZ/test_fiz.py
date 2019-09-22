7a5348267d7fa6d4860c572640f94e78d259da1a
sbx_user1051 <sbx_user1051@169.254.121.93>
2019-09-22 20:01:11 +0000

    Sun Sep 22 20:01:11 UTC 2019

--- test/solution_tests/FIZ/test_fiz.py
+++ test/solution_tests/FIZ/test_fiz.py
@@ -1,6 +1,11 @@
-from solutions.HLO import hello_solution
+from solutions.FIZ import fizz_buzz_solution
+
+class TestFiz():
+    def test_fiz(self):
+        assert fizz_buzz_solution.fizz_buzz(3) == 'fizz'
+        assert fizz_buzz_solution.fizz_buzz(9) == 'fizz'
+        assert fizz_buzz_solution.fizz_buzz(20) == 'buzz'
+        assert fizz_buzz_solution.fizz_buzz(15) == 'fizz buzz'
+        assert fizz_buzz_solution.fizz_buzz(11) == 11
 
 
-class TestSum():
-    def test_hello(self):
-        assert hello_solution.hello('Andy') == ('Hello, Andy!')
