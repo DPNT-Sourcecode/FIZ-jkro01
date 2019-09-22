30c11685a2bbf1f01b71b2879bf4e49d253df066
sbx_user1051 <sbx_user1051@169.254.42.237>
2019-09-22 20:52:58 +0000

    Sun Sep 22 20:52:58 UTC 2019

--- test/solution_tests/FIZ/test_fiz.py
+++ test/solution_tests/FIZ/test_fiz.py
@@ -2,14 +2,15 @@ from solutions.FIZ import fizz_buzz_solution
 
 class TestFiz():
     def test_fiz(self):
-        assert fizz_buzz_solution.fizz_buzz(3) == 'fizz'
+        assert fizz_buzz_solution.fizz_buzz(3) == 'fizz fake deluxe'
         assert fizz_buzz_solution.fizz_buzz(9) == 'fizz'
         assert fizz_buzz_solution.fizz_buzz(20) == 'buzz'
-        assert fizz_buzz_solution.fizz_buzz(15) == 'fizz buzz'
-        assert fizz_buzz_solution.fizz_buzz(11) == 'fake deluxe'
+        assert fizz_buzz_solution.fizz_buzz(15) == 'fizz buzz fake deluxe'
+        assert fizz_buzz_solution.fizz_buzz(11) == '11'
         assert fizz_buzz_solution.fizz_buzz(301) == 'fizz'
         assert fizz_buzz_solution.fizz_buzz(551) == 'buzz'
         assert fizz_buzz_solution.fizz_buzz(33) == 'fizz fake deluxe'
         assert fizz_buzz_solution.fizz_buzz(55) == 'buzz fake deluxe'
+
 
 
